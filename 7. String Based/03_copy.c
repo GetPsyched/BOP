7325d7d6b5adbef61ff69ddcdbd230db82c6d56f
GetPsyched <priyanshutr@gmail.com>
2021-01-23 15:53:28 +0400

    Fixed error with copy

--- 7. String Based/03_copy.c
+++ 7. String Based/03_copy.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-  int count = 0;
+  int i;
   char x[25], y[25];
   printf("Enter a string --> ");
   gets(x);
-  for(int i=0; x[i] != '\0'; i++)
+  for(i=0; x[i] != '\0'; i++)
     y[i] = x[i];
+  y[i] = '\0';
   printf("Copied string --> %s\n", y);
 }
