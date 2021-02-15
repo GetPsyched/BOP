349b99ae0ac93da3e5445e1548b13c4c6bc4e381
GetPsyched <priyanshutr@gmail.com>
2021-02-15 13:04:36 +0400

    Initial Commit

--- 8. Structure Based/2.c
+++ 8. Structure Based/2.c
@@ -3,51 +3,72 @@
 int mark_avg=0;
 
 struct student{
-    char name[30];
-    int rno;
-    int marks;
-    int fee;
+  char name[30];
+  int rno;
+  int marks;
+  int fee;
 };
 
 void Topper(struct student st[],int n){
-    int max=0;
-    for(int i=0;i<n;i++){
-        if(st[max].marks<st[i].marks) max=i;
-    }
-    printf("Details of the topper : %s %d %d %d",st[max].name,st[max].rno,st[max].marks,st[max].fee);
+  int max=0;
+  for(int i=0;i<n;i++){
+    if(st[max].marks<st[i].marks) max=i;
+  }
+  printf("\nDetails of the topper:\n");
+  printf("\nName --> %s", st[max].name);
+  printf("\nRoll --> %d", st[max].rno);
+  printf("\nMarks --> %d", st[max].marks);
+  printf("\nFee --> %d\n", st[max].fee);
 }
 
 void greater(struct student st[],int n){
-    printf("Students having marks greater than avg marks : \m");
-    for(int i=0;i<n;i++){
-        if(st[i].marks>mark_avg)
-        printf("%s %d %d %d\n",st[i].name,st[i].rno,st[i].marks,st[i].fee);
+  printf("\nStudents having marks greater than avg marks:\n");
+  for(int i=0;i<n;i++){
+    if(st[i].marks>mark_avg){
+      printf("\nName --> %s", st[i].name);
+      printf("\nRoll --> %d", st[i].rno);
+      printf("\nMarks --> %d", st[i].marks);
+      printf("\nFee --> %d\n", st[i].fee);
     }
+  }
 }
 
 void FEE(struct student st[],int n){
-    printf("Students who have not paid there fees : \m");
-    for(int i=0;i<n;i++){
-        if(st[i].fee<=0)
-        printf("%s %d %d %d\n",st[i].name,st[i].rno,st[i].marks,st[i].fee);
+  int flag = 0;
+  printf("\nStudents who have not paid their fees: ");
+  for(int i=0;i<n;i++){
+    if(st[i].fee<=0){
+      flag = 1;
+      printf("\nName --> %s", st[i].name);
+      printf("\nRoll --> %d", st[i].rno);
+      printf("\nMarks --> %d", st[i].marks);
+      printf("\nFee --> %d\n", st[i].fee);
     }
+  if (flag == 0)
+    printf("None");
+  }
 }
 
 int main(){
-    int n;
-    printf("Enter the number of students : ");
-    scanf("%d",&n);
-    struct student st[n];
-    printf("Enter the details like : name rno marks fee\n");
-    for(int i=0;i<n;i++){
-        printf("Student %d : ",i+1);
-        scanf("%s%d%d%d",&st[i].name,&st[i].rno,&st[i].marks,&st[i].fee);
-        mark_avg+=st[i].marks;
-    }
-    mark_avg/=n;
-    greater(st,n);
-    Topper(st,n);
-    FEE(st,n);
-    printf("\n");
-    return 0;
+  int n;
+  printf("Enter the number of students --> ");
+  scanf("%d",&n);
+  struct student st[n];
+  for(int i=0;i<n;i++){
+    printf("\nStudent %d:\n",i+1);
+    printf("Name --> ");
+    scanf("%s", &st[i].name);
+    printf("Roll --> ");
+    scanf("%d", &st[i].rno);
+    printf("Marks --> ");
+    scanf("%d", &st[i].marks);
+    printf("Fee --> ");
+    scanf("%d", &st[i].fee);
+    mark_avg+=st[i].marks;
+  }
+  mark_avg/=n;
+  greater(st,n);
+  Topper(st,n);
+  FEE(st,n);
+  return 0;
 }
