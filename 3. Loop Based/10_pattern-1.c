aba91881193456f8b35d941f4a908aebb2004fc4
GetPsyched <priyanshutr@gmail.com>
2021-03-07 13:02:52 +0400

    Fixed printing error

--- 3. Loop Based/10_pattern-1.c
+++ 3. Loop Based/10_pattern-1.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-  int n;
+  int n, i, j;
   printf("Enter n --> ");
   scanf("%d", &n);
-  for (int i = 1 ; i <= n ; i++)
+  for (i = 1 ; i <= n ; i++)
     {
-      for (int j = 1 ; j <= i ; j++)
-        printf("%d", &n);
+      for (j = 1 ; j <= i ; j++)
+        printf("*");
       printf("\n");
     }
 }
